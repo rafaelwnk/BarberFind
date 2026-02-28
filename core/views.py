1ef590cd9547b6186792a3b9f92a6abd3b578cfa
rafaelwnk <rafaelcarmo749@gmail.com>
2026-02-27 22:17:28 -0300

    adicionando templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'core/login.html')
+
+def register_view(request):
+    return render(request, 'core/register.html')
+
+def home_view(request):
+    return render(request, 'core/home.html')
+
+def search_view(request):
+    return render(request, 'core/search.html')
+
+def history_view(request):
+    return render(request, 'core/history.html')
